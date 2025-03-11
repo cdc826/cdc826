4182d6c6ab5bce513a8d3061fff9ecfa48cd804e
Taeyeong Kim <ktylp0728@gmail.com>
2025-03-11 13:03:03 +0900

    Init: 프로젝트 생성

---
+++ backend/src/main/java/com/ssafy/homebody/HomebodyApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.homebody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomebodyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomebodyApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/ssafy/homebody/HomebodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.homebody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomebodyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
